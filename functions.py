9dab0085ed5be5b03ff1025e16e74a6b29b0d17c
Mors <seyedsahel1383@gmail.com>
2024-08-15 00:53:16 +0330

    get repo languages

--- functions.py
+++ functions.py
@@ -19,7 +19,34 @@ def get_repo_names(username):
         return []
 
 
-username = "tahamusvi"
-repo_names = get_repo_names(username)
+def get_repo_languages(username):
+    repos_url = f"https://api.github.com/users/{username}/repos"
+    
+    response = requests.get(repos_url)
+    
+    if response.status_code == 200:
+        repos = response.json()
+        
+        repo_languages = {}
+        
+        for repo in repos:
+            repo_name = repo['name']
+            
+            lang_url = repo['languages_url']
+            lang_response = requests.get(lang_url)
+            
+            if lang_response.status_code == 200:
+                languages = lang_response.json()
+                main_language = max(languages, key=languages.get, default="Unknown")
+                repo_languages[repo_name] = main_language
+            else:
+                repo_languages[repo_name] = "Error retrieving languages"
+        
+        return repo_languages
+    else:
+        print(f"Error: {response.status_code}")
+        return {}
 
-print(repo_names)
+username = "tahamusvi"
+repo_languages = get_repo_languages(username)
+print(repo_languages)
