2303690ce5987aa98e406cf7814ca49634229429
Mors <seyedsahel1383@gmail.com>
2024-08-15 23:58:48 +0330

    combine two files , edit get_repo_languages func ,changing language_count into sorted

--- functions.py
+++ functions.py
@@ -1,31 +1,12 @@
 import requests
 
-def get_repo_names(username):
-    url = f"https://api.github.com/users/{username}/repos"
-    print(url)
-
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    'Accept-Language': 'en-US,en;q=0.5',
-    }       
-    response = requests.get(url , headers = headers)
-    print(response)
-
-    if response.status_code == 200:
-        repos = response.json()
-        
-        repo_names = [repo['name'] for repo in repos]
-        
-        return repo_names
-    else:
-        print(f"Error: {response.status_code}")
-        return []
 
 
-def get_repo_languages(username):
+def get_repo_languages(username,token):
     repos_url = f"https://api.github.com/users/{username}/repos"
+    headers = {'Authorization': f'token {token}'}
     
-    response = requests.get(repos_url)
+    response = requests.get(repos_url,headers = headers)
     
     if response.status_code == 200:
         repos = response.json()
@@ -34,18 +15,10 @@ def get_repo_languages(username):
         
         for repo in repos:
             repo_name = repo['name']
-            
-            lang_url = repo['languages_url']
-            lang_response = requests.get(lang_url)
-            
-            if lang_response.status_code == 200:
-                languages = lang_response.json()
-                main_language = max(languages, key=languages.get, default="Unknown")
-                # if main_language == "Unknown":
-                #     continue
-                repo_languages[repo_name] = main_language
-            else:
-                repo_languages[repo_name] = "Error retrieving languages"
+            language = repo.get('language', 'No language specified')
+            if language == None :
+                continue
+            repo_languages[repo_name] = language
         
         return repo_languages
     else:
@@ -61,11 +34,58 @@ def count_repos_by_language(repo_languages):
         else:
             language_count[language] = 1
             
+    language_count = dict(sorted(language_count.items(), key=lambda item: item[1], reverse=True))
+            
     return language_count
 
+
+def get_latest_activity(username, token):
+    url = f"https://api.github.com/users/{username}/events"
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        events = response.json()
+        filtered_events = []
+
+        for event in events:
+            event_type = event['type']
+            if event_type in ['PushEvent', 'CreateEvent', 'PullRequestEvent']:
+                filtered_events.append(event)
+
+        if filtered_events:
+            latest_event={}
+            latest_event['type'] = filtered_events[0]['type'] 
+            latest_event['repo_name'] = filtered_events[0]['repo']['name'] 
+            latest_event['repo_url'] = filtered_events[0]['repo']['url'] 
+            latest_event['repo_lang'] = get_repository_language(latest_event['repo_name'], token)
+            return latest_event
+        else:
+            return "No relevant activities found for this user."
+    else:
+        return f"Error fetching activities: {response.status_code}"
+
+def get_repository_language(repo_name, token):
+    url = f"https://api.github.com/repos/{repo_name}"
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        repo_info = response.json()
+        language = repo_info.get('language', 'No language specified')
+        return language
+    else:
+        return f"Error fetching repository info: {response.status_code}"
+
+
 username = "tahamusvi"
-repo_languages = get_repo_languages(username)
+
+repo_languages = get_repo_languages(username,token)
 print(repo_languages)
 
 language_count = count_repos_by_language(repo_languages)
 print(language_count)
+
+latest_activity = get_latest_activity(username, token)
+print(latest_activity)
+
